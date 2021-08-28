fd57db3ce3bffe01081e9abd1454e75e7f8c4859
Alejandro Gonzalez <alegonzo97@gmail.com>
2021-08-28 11:25:39 -0400

    refact payment controller

--- src/modules/payment/controllers/payment.controller.ts
+++ src/modules/payment/controllers/payment.controller.ts
@@ -1,21 +1,12 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { PaymentService } from '../services/payment.service';
 import { EntumovilDto } from '../dto/entumovil.dto';
-import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 import { EventPattern } from '@nestjs/microservices';
 import { CREATE_ENTUMOVIL_PAYMENT } from '../utils/constants';
 
 @Controller('payments')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
-
-  /*@UseGuards(ThrottlerGuard)
-  @Throttle(1000, 60)
-  @Post('entumovil')
-  async entumovilPayment(@Body() entumovilDto: EntumovilDto) {
-    await this.paymentService.saveEntumovilPayment(entumovilDto);
-    return { status: 200, message: 'Pago registrado correctamente' };
-  }*/
 
   @EventPattern(CREATE_ENTUMOVIL_PAYMENT)
   async createEntumovilPayment(data: EntumovilDto) {
